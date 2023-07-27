e59203908d3f7a407d6ffb1ae3ad4c3cb7996cb0
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-07-27 20:44:12 +0100

    rename all

--- src/main/java/org/developerden/codosseum/Application.java
+++ src/main/java/org/developerden/codosseum/Application.java
@@ -1,4 +1,4 @@
-package org.developerden;
+package org.developerden.codosseum;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
--- src/test/java/org/developerden/codosseum/CodosseumTest.java
+++ src/test/java/org/developerden/codosseum/CodosseumTest.java
@@ -1,4 +1,4 @@
-package org.developerden;
+package org.developerden.codosseum;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
