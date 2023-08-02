3461223ce1d37b21e23bdbc2ef8ed4166b9dfb71
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-08-02 18:07:15 +0200

    Fix checkstyle violations in source

--- src/main/java/org/developerden/codosseum/Application.java
+++ src/main/java/org/developerden/codosseum/Application.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
+
 package org.developerden.codosseum;
 
 import io.micronaut.runtime.Micronaut;
@@ -21,14 +22,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(
-        info = @Info(
-                title = "codosseum",
-                version = "0.0"
-        )
+    info = @Info(
+        title = "codosseum",
+        version = "0.0"
+    )
 )
 public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    Micronaut.run(Application.class, args);
+  }
 }
