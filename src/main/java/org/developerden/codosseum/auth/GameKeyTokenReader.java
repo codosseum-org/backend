3be276c688109250477524d80237dabaa83771d1
JohnnyJayJay <johnnyjayjay02@gmail.com>
2023-08-19 18:03:52 +0200

    Add authentication class stubs

---
+++ src/main/java/org/developerden/codosseum/auth/GameKeyTokenReader.java
@@ -0,0 +1,34 @@
+/*
+ * SPDX-FileCopyrightText: 2023 JohnnyJayJay
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.developerden.codosseum.auth;
+
+import io.micronaut.security.token.reader.HttpHeaderTokenReader;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class GameKeyTokenReader extends HttpHeaderTokenReader {
+  @Override
+  protected String getPrefix() {
+    return "Game ";
+  }
+
+  @Override
+  protected String getHeaderName() {
+    return "Authorization";
+  }
+}
