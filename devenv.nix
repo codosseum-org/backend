3ed164f7cd156ac44825d95364a9888e90ae3cdf
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-08-28 13:34:37 +0100

    tidy up devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -4,16 +4,8 @@
 
   packages = with pkgs; [ git pre-commit checkstyle nixfmt ];
 
-  # https://devenv.sh/languages/
-   languages.nix.enable = true;
-   languages.java.enable = true;
-   languages.java.gradle.enable = true;
+  languages.nix.enable = true;
+  languages.java.enable = true;
+  languages.java.gradle.enable = true;
 
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
