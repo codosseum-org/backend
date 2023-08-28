9bf45bc872632fd106f78f8ab89dcee83f4782c9
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-08-28 13:31:12 +0100

    add actual devenv config

---
+++ devenv.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+{
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.pre-commit pkgs.checkstyle ];
+
+  # https://devenv.sh/languages/
+   languages.nix.enable = true;
+   languages.java.enable = true;
+   languages.java.gradle.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
