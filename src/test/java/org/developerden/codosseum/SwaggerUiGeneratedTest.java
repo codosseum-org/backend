2c87f2da1992a2dfb08650e6d3738921573948f2
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-10-08 12:39:27 +0100

    make all code REUSE compliant

--- src/test/java/org/developerden/codosseum/SwaggerUiGeneratedTest.java
+++ src/test/java/org/developerden/codosseum/SwaggerUiGeneratedTest.java
@@ -1,16 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Alexander Wood (BristerMitten)
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.developerden.codosseum;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-@MicronautTest(startApplication = false) 
+@MicronautTest(startApplication = false)
 class SwaggerUiGeneratedTest {
 
-    @Test
-    void buildGeneratesOpenApi(ResourceLoader resourceLoader) {
-        assertTrue(resourceLoader.getResource("META-INF/swagger/views/swagger-ui/index.html").isPresent());
-    }
+  @Test
+  void buildGeneratesOpenApi(ResourceLoader resourceLoader) {
+    assertTrue(
+        resourceLoader.getResource("META-INF/swagger/views/swagger-ui/index.html").isPresent());
+  }
 }
