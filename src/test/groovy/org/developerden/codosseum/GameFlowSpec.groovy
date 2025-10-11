6dee4972c702a788c521537a508588058e12fb51
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-10-11 22:26:12 +0100

    improve GameFlowSpec, checking for events

--- src/test/groovy/org/developerden/codosseum/GameFlowSpec.groovy
+++ src/test/groovy/org/developerden/codosseum/GameFlowSpec.groovy
@@ -14,21 +14,54 @@
 
 package org.developerden.codosseum
 
+
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.sse.Event
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.developerden.codosseum.dto.*
+import org.developerden.codosseum.event.GameEvent
+import org.developerden.codosseum.event.RoundStartEvent
+import org.developerden.codosseum.event.SyncEvent
 import org.developerden.codosseum.mode.GameModeType
 import org.developerden.codosseum.model.GamePhase
+import org.developerden.codosseum.service.game.event.SseEventSink
+import org.reactivestreams.Subscription
+import reactor.core.publisher.BaseSubscriber
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+import java.util.concurrent.CopyOnWriteArrayList
 
 @MicronautTest
 class GameFlowSpec extends Specification {
     @Inject
     @Client("/")
     HttpClient http
+
+    @Inject
+    SseEventSink eventSink
+
+    // has to be thread safe as events come from another thread
+    List<GameEvent> eventsReceived = new CopyOnWriteArrayList<>()
+
+
+    void setup() {
+        eventsReceived.clear()
+        eventSink.allEvents().subscribe(new BaseSubscriber<Event<GameEvent>>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(Long.MAX_VALUE)
+            }
+
+            @Override
+            protected void hookOnNext(Event<GameEvent> value) {
+                eventsReceived.add(value.data)
+            }
+        })
+    }
 
     def "Warmup phase starts correctly"() {
         when:
@@ -54,8 +87,15 @@ class GameFlowSpec extends Specification {
         then:
         getResponse.status.code == 204
 
-        when:
-        sleep(5000)
+        and: "wait until both events arrive"
+        def conditions = new PollingConditions(timeout: 6, initialDelay: 0.1, delay: 0.1)
+        conditions.eventually {
+            assert eventsReceived.size() >= 2
+            assert eventsReceived[0] instanceof SyncEvent
+            assert (eventsReceived[0] as SyncEvent).state() == GamePhase.WARMUP
+            assert eventsReceived[1] instanceof RoundStartEvent
+        }
+
 
         def infoResponse = http.toBlocking().exchange("/games/${gameId}", GameInfo)
         then:
@@ -64,5 +104,6 @@ class GameFlowSpec extends Specification {
         info.id() == gameId
         info.state() == GamePhase.IN_PROGRESS
         info.players().allPlayers().count() == 1
+
     }
 }
